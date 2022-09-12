f78bc63bf612bdc44b792888cb4cbb3e0e1850c2
Gui Recordon <guilherme.recordon@gmail.com>
2022-09-12 16:48:48 -0300

    Set up global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-function App() {
+import { ThemeProvider } from "styled-components"
+import { GlobalStyle } from "./styles/global"
+import { defaultTheme } from "./styles/themes/default"
+
+export function App() {
   return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
     <h1>Hello, world!</h1>
+    </ThemeProvider>
   )
 }
 
-export default App
