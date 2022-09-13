1af7d4129186fed091a0d0d7cdb1545ce4ec12fa
Gui Recordon <guilherme.recordon@gmail.com>
2022-09-13 20:10:46 -0300

    Finish issue card component

---
+++ src/Components/IssueCard/index.tsx
@@ -0,0 +1,16 @@
+import { CardContainer, CardHeader } from "./styles";
+
+export function IssueCard() {
+  return (
+    <CardContainer>
+      <CardHeader>
+        <h3>JavaScript data types and data structures</h3>
+        <span>Há 1 dia</span>
+      </CardHeader>
+
+      <div>
+        Programming languages all have built-in data structures, but these often differ from one language to another. This article attempts to list the built-in data structures available in 
+      </div>
+    </CardContainer>
+  )
+}
--- src/pages/Blog/index.tsx
+++ src/pages/Blog/index.tsx
@@ -1,11 +1,21 @@
 import { Author } from "../../Components/Author";
+import { IssueCard } from "../../Components/IssueCard";
 import { SearchForm } from "../../Components/SearchForm";
+import { CardsGrid } from "./styles";
 
 export function Blog() {
   return (
     <div>
       <Author />
       <SearchForm />
+      <CardsGrid>
+        <IssueCard />
+        <IssueCard />
+        <IssueCard />
+        <IssueCard />
+        <IssueCard />
+        <IssueCard />
+      </CardsGrid>
     </div>
   )
 }
